b02420f0472c147f8066f3dd1a3c0aeae6c8db8f
Brandon Brigance <brandon.brigance@gmail.com>
2016-07-15 01:01:53 -0500

    added devise gem/ configured

--- Gemfile
+++ Gemfile
@@ -52,3 +52,5 @@ group :production do
 
 	gem 'rails_12factor'
 end
+
+gem 'devise'
--- app/models/concerns/order.rb
+++ app/models/concerns/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
 	belongs_to :product
+	belongs_to :user
 end
 #because each order can only include one product
