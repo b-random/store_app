cf392ab3e2f6b8aa0c516206bdc9824aebd5941c
Brandon Brigance <brandon.brigance@gmail.com>
2016-07-26 18:25:33 -0500

    styling raty plugin

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,8 @@
 /*any new JS files you put into this folder should be included by your application automatically- this file will contain site wide functions*/
 
 $(document).on('ready page:load', function() {
-	$('.rating').raty;
+	$('.rating').raty( { path: '/assets/images' });
 });
 
-/* 
+/* When the page is loaded raty will init. on the rating class*/
+/* path: method points up to the assets folder to find images */
