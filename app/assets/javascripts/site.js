cf8067b7e523e717b7b5f5def9d49f86a1e83696
Brandon Brigance <brandon.brigance@gmail.com>
2016-07-27 00:50:55 -0500

    styled jq raty plugin

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,12 @@
 /*any new JS files you put into this folder should be included by your application automatically- this file will contain site wide functions*/
 
 $(document).on('ready page:load', function() {
-	$('.rating').raty( { path: '/assets/images' });
-});
+	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
 
 /* When the page is loaded raty will init. on the rating class*/
 /* path: method points up to the assets folder to find images */
+  $('.rated').raty({ path: '/assets/images', readOnly: true, score: function() {
+  	  return $(this).attr('data-score');
+    }
+  });
+});
