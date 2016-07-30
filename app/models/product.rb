35b1a2b1f83c64024282e9fb781d75c98b66ab9a
Brandon Brigance <brandon.brigance@gmail.com>
2016-07-30 13:46:08 -0500

    comment validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
+  #this sends an error if a name is NOT included when creating a new product, through the product _form
 
 	has_many :orders
 	has_many :comments
