86762c5fe37aa101044291bbe315ef5e49007ce4
Brandon Brigance <brandon.brigance@gmail.com>
2016-09-05 17:50:26 -0500

    generate js instead of coffeescript in development

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -2,3 +2,5 @@ module ApplicationCable
   class Channel < ActionCable::Channel::Base
   end
 end
+
+# You can use channel.rb to define global methods for all all the channels.
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,3 +2,5 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
   end
 end
+
+#The connection.rb file can be used to run any sort of code before the connection between a user and the server is established
