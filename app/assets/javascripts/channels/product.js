f599547ce50f4f151f742e0ee315becb7c6273a7
Brandon Brigance <brandon.brigance@gmail.com>
2016-09-05 19:13:03 -0500

    configure changes for heroku

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -9,5 +9,11 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
+    $(".alert.alert-info").show();
   }
 });
+
+
+
+
+// This code is a little more complex but just as self-explanatory. App.product = App.cable.subscriptions.create("ProductChannel", { }); is the function that subscribes a user to a specific channel and assignes it to the variable App.product. This makes the current subscription available through App.product. Whenever this line is executed, the subscription is created, and the subscribed method in the product_channel.rb is called. So this is the JavaScript part that tells your server that a new subscriber is connected. The first part of the create method tells your app which channel to connect to. The second part is an object with a few predefined properties. connected is activated the moment a user successfully connected to the server. disconnected is the obvious counterpart. received will be executed whenever the client receives any data from the server. So in the example of a chat, this function would always be triggered when another user sends a message.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://beardco-store-app.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://beardco-store-app.herokuapp.com', 'http://beardco-store-app.herokuapp.com']
 end
