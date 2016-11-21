19f0f7d70e3a8941218b4b16d557fab273620efd
Brandon Brigance <brandon.brigance@gmail.com>
2016-11-20 21:10:14 -0600

    star rating fix

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,11 +2,11 @@
 
 $(document).on('turbolinks:load', function() {
 	$('.img-zoom').elevateZoom({scrollZoom: true});
-	$('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
+	$('.rating').raty( { path: '/assets/images/images', scoreName: 'comment[rating]' });
 
 /* When the page is loaded raty will init. on the rating class*/
 /* path: method points up to the assets folder to find images */
-  $('.rated').raty({ path: '/assets/images', readOnly: true, score: function() {
+  $('.rated').raty({ path: '/assets/images/images', readOnly: true, score: function() {
   	  return $(this).attr('data-score');
     }
   });
